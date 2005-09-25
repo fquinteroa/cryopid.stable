e8077a2d387dfb738513d6dfc9bd3f1bdf25958e
dagobah <dagobah@localhost>
2005-09-25 04:52:57 +0000

    Whoops! x86 has no rip

--- cryopid/trunk/src/arch-i386/process.c
+++ cryopid/trunk/src/arch-i386/process.c
@@ -186,7 +186,7 @@ int is_a_syscall(unsigned long inst, int canonical)
 int is_in_syscall(pid_t pid, struct user *user)
 {
     long inst;
-    inst = ptrace(PTRACE_PEEKDATA, pid, user->regs.rip-2, 0);
+    inst = ptrace(PTRACE_PEEKDATA, pid, user->regs.eip-2, 0);
     if (errno) {
 	perror("ptrace(PEEKDATA)");
 	return 0;
