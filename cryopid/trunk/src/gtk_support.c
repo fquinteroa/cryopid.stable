57bbe07b2ffcb6d304c0ea970364ddeec5134ed9
dagobah <dagobah@localhost>
2005-11-13 14:48:16 +0000

    Fork out PLT code

--- cryopid/trunk/src/gtk_support.c
+++ cryopid/trunk/src/gtk_support.c
@@ -1,9 +1,20 @@
+#if 1
 #include <gtk/gtk.h>
+#else
+/* We don't really need Gtk devel libraries do we? */
+#define GdkDisplay void
+#define GdkDisplayManager void
+#define GdkScreen void
+#define GtkWindow void
+#define GdkWindow void
+#define GList void
+#define GType void*
+#define GTypeInstance void
+#define GFunc void*
+#define GTK_IS_WINDOW(x) (1) /* Bleh, live with the assertions */
+#endif
+
 #include <signal.h>
-#include <string.h>
-#include <elf.h>
-#include <link.h>
-#include <sys/types.h>
 #include <unistd.h>
 
 #include "cryopid.h"
@@ -12,101 +23,15 @@
 char display_environ[80];
 int need_gtk = 0;
 
-static void* resolve(struct link_map *l, char *what)
-{
-    struct link_map *lm;
-    Elf32_Dyn *dyn;
-    Elf32_Sym *sym;
-    char *strtab;
-
-    void *val = NULL;
-    
-    for (lm = l; lm != NULL; lm = lm->l_next) {
-	sym = NULL;
-	strtab = NULL;
-
-	for (dyn = (Elf32_Dyn*)(lm->l_ld); dyn->d_tag != DT_NULL; dyn++) {
-	    if (dyn->d_tag == DT_STRTAB)
-		strtab = (char *)(dyn->d_un.d_ptr);
-	    if (dyn->d_tag == DT_SYMTAB)
-		sym = (Elf32_Sym *)(dyn->d_un.d_ptr);
-	}
-	
-	while (sym) {
-	    if (sym->st_name > 0x100000)
-		break;
-	    if (strcmp(strtab + sym->st_name, what) == 0) {
-		val = (void*)(lm->l_addr + sym->st_value);
-		if (sym->st_value) {
-		    /*
-		    debug("--> we have found %s @ 0x%08x (0x%08lx)",
-			    strtab + sym->st_name, sym->st_value,
-			    (unsigned long)(lm->l_addr + sym->st_value));
-			    */
-		    return val;
-		}
-	    }
-	    sym++;
-	}
-    }
-    if (val == NULL)
-	fprintf(stderr, "Argh! Couldn't find %s in binary\n", what);
-    return val;
-}
-
-static void* find_lm()
-{
-    Elf32_Ehdr *elf;
-    Elf32_Phdr *phdr;
-    Elf32_Dyn *dyn;
-    int i, cnt;
-    unsigned long *got;
-    struct link_map *lm;
-    /* FIXME: find dynamically... preferably from reading image */
-    //elf = (Elf32_Ehdr *)((unsigned int)(main) & 0xfffff000);
-    elf = (Elf32_Ehdr*)0x8048000;
-    phdr = (Elf32_Phdr *)((unsigned char *)(elf) + elf->e_phoff);
-    
-    for (i = 0; i < elf->e_phnum; i++)
-	if (phdr[i].p_type == PT_DYNAMIC)
-	    break;
-
-    if (i == elf->e_phnum) {
-	printf("Not a dynamic elf file?\n");
-	return NULL;
-    }
-
-    phdr += i;
-
-    dyn = (Elf32_Dyn *)(phdr->p_vaddr);
-    cnt = phdr->p_filesz / sizeof(Elf32_Dyn);
-
-    got = NULL;
-    for (i = 0; i < cnt; i++)
-	if (dyn[i].d_tag == DT_PLTGOT)
-	    got = (unsigned long *)(dyn[i].d_un.d_ptr);
-
-    if (got == NULL) {
-	printf("Unable to find GOT\n");
-	return NULL;
-    }
-
-    lm = (struct link_map *)(got[1]);
-    
-    //debug("link_map @ 0x%08lx", (unsigned long)lm);
-
-    return lm;
-}
-
 void cryopid_migrate_gtk_windows()
 {
-    void *lm = find_lm();
+    void *lm = find_linkmap((void*)0x8048000); /* FIXME: find dynamically */
 #define find_symbol(name, rettype, params) \
-    	rettype (*_##name)params = resolve(lm, #name); \
+    	rettype (*_##name)params = plt_resolve(lm, #name); \
     	if (_##name == NULL) \
 	    return;
 #define find_symbol_noprefix(name, rettype, params) \
-    	rettype (*name)params = resolve(lm, #name); \
+    	rettype (*name)params = plt_resolve(lm, #name); \
     	if (name == NULL) \
 	    return;
     find_symbol(gdk_display_get_default, GdkDisplay*, ());
