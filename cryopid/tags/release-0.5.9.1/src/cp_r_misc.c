ed749f5242077853b1a370a2881a3bfcf74c9797
Tim Post <echo@echoreply.us>
2009-04-17 11:32:11 +0800

    Move last tagged release (0.5.9.1) to repository root

--- cryopid/tags/release-0.5.9.1/src/cp_r_misc.c
+++
@@ -1,8 +0,0 @@
-#include "cpimage.h"
-
-void read_chunk_misc(void *fptr, int action)
-{
-    /* FIXME */
-}
-
-/* vim:set ts=8 sw=4 noet: */
--- cryopid/tags/release-0.5.9/src/cp_r_header.c
+++
@@ -1,9 +0,0 @@
-
-#include "cryopid.h"
-#include "cpimage.h"
-
-void read_chunk_header(void *fptr, int action)
-{
-}
-
-/* vim:set ts=8 sw=4 noet: */
